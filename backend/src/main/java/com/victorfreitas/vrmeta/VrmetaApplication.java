20d1328021d86ab06da84eb722f5fa2fc376ab8a
Victor Rodrigues <victorxdrodrigues@gmail.com>
2022-07-12 10:28:17 -0300

    Project created

---
+++ backend/src/main/java/com/victorfreitas/vrmeta/VrmetaApplication.java
@@ -0,0 +1,13 @@
+package com.victorfreitas.vrmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/victorfreitas/vrmeta/VrmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorfreitas.vrmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
