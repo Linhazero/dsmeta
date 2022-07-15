72ed4e5bbbb99fd9c0135b08dbef75e9259b69dd
Victor Rodrigues <victorxdrodrigues@gmail.com>
2022-07-15 16:20:12 -0300

    API test

---
+++ backend/src/main/java/com/victorfreitas/vrmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.victorfreitas.vrmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.victorfreitas.vrmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
---
+++ backend/src/main/java/com/victorfreitas/vrmeta/services/SaleService.java
@@ -0,0 +1,21 @@
+package com.victorfreitas.vrmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.victorfreitas.vrmeta.entities.Sale;
+import com.victorfreitas.vrmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSales() {
+		return repository.findAll();
+	}
+
+}
