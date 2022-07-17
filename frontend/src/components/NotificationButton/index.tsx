1d94d1a33c1f74803315de107af69d514a9ae2db
Victor Rodrigues <victorxdrodrigues@gmail.com>
2022-07-17 15:28:41 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,23 @@
+import axios from 'axios';
 import incon from '../../assets/img/notification-icon.svg'
+import { BASE_URL } from '../../utils/request';
 import './styles.css'
 
-function NotificationButton() {
+type props = {
+  saleId: number;
+}
+ 
+
+function hanledClick(id: number){
+  axios(`${BASE_URL}/sales/${id}/notication`)
+  .then(Response => {
+    console.log("Sucesso");
+  })
+}
+
+function NotificationButton( {saleId} : props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => hanledClick(saleId)}>
         <img src={incon} alt="Notificar" />
       </div>
     )
